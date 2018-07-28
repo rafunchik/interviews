b9304846453446c7b188e39cc62c1597e0c96406
rafael.castro <rafael.castro@schibsted.com>
2018-07-28 18:28:55 +0200

    first commit, working solution Scala, README needs polishing

---
+++ url-shortener/src/test/scala/io/github/rafunchik/urlshortener/shorturl/urlgenerator/ShortUrlGeneratorSpec.scala
@@ -0,0 +1,25 @@
+package io.github.rafunchik.urlshortener.shorturl.urlgenerator
+
+import org.specs2.mutable.Specification
+
+class ShortUrlGeneratorSpec extends Specification {
+
+  "ShortUrlGeneratorSpec" should {
+    "generate a different String of 10 alphanumerical chars every time" in {
+      val url = "www.google.com"
+
+      val generated = Set(
+        ShortUrlGenerator.generateString(url),
+        ShortUrlGenerator.generateString(url),
+        ShortUrlGenerator.generateString(url))
+
+      generated.size must beEqualTo(3)
+      forall(generated) {
+        url => {
+          url.length must beEqualTo(10)
+          url must not contain "/"
+        }
+      }
+    }
+  }
+}
